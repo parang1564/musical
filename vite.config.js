5cf4c204dd6e136f1289a3f8834f19b308d81033
ssuhyun <hssuhyun@gmail.com>
2026-08-24 22:46:29 +0900

    업데이트

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // 수정: './' 대신 '/musical/'을 입력해야 올바르게 작동합니다.
-  base: '/musical/', 
   plugins: [
     react(),
     tailwindcss(),
