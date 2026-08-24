9ab88add9e9fa3f733f068358a074a0d17ca5f42
ssuhyun <hssuhyun@gmail.com>
2026-08-24 22:22:19 +0900

    업데이트

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/', // 혹은 './'
+  base: '/musical-scheduler/', // 👈 본인의 실제 깃허브 레포지토리 이름 (앞뒤로 슬래시 필수)
   plugins: [
     react(),
     tailwindcss(),
